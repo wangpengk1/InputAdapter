c01dba6731da791b8a0fdef06ed1a0e816aceb06
wangpengk1 <wangpengk1@163.com>
2021-03-23 14:47:22 +0800

    2021-03-23

--- BaseInputAdapter/src/main/java/com/newasia/baseinputadapter/GetImageHelper.java
+++ BaseInputAdapter/src/main/java/com/newasia/baseinputadapter/GetImageHelper.java
@@ -2,32 +2,23 @@ package com.newasia.baseinputadapter;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.ClipData;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.database.Cursor;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.os.Build;
-import android.os.Environment;
 import android.provider.MediaStore;
-
-
-import androidx.core.content.FileProvider;
-
-
-
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
+import java.util.List;
 
 
 import static android.app.Activity.RESULT_OK;
 
-public class GetImageHelper
-{
+public class GetImageHelper {
     private Activity mContext;
     private static final String PERMISSION_CAMERA = Manifest.permission.CAMERA;
     private static final String PERMISSION_WRITE = Manifest.permission.WRITE_EXTERNAL_STORAGE;
@@ -36,52 +27,40 @@ public class GetImageHelper
     private static final int CROP_REQUEST_CODE = 0x003;
 
 
-    private File captureFile;
-    private File cropFile;
-    private File rootFile;
+    private Uri mTakePhotoUri;
+    private Uri mCorpUri;
 
     private boolean mIsCrop;
     private onTakeImageResult mOnResult;
 
-    public Activity getContext()
-    {
+    public Activity getContext() {
         return mContext;
     }
 
     private static GetImageHelper mInstance;
 
-    public static GetImageHelper getInstance(boolean isCrop,Activity context)
-    {
-        if(mInstance!=null)
-        {
-            if(mInstance.getContext()!= context)
-            {
-                mInstance = new GetImageHelper(isCrop,context);
+    public static GetImageHelper getInstance(boolean isCrop, Activity context) {
+        if (mInstance != null) {
+            if (mInstance.getContext() != context) {
+                mInstance = new GetImageHelper(isCrop, context);
             }
-        }
-        else  mInstance = new GetImageHelper(isCrop,context);
+        } else mInstance = new GetImageHelper(isCrop, context);
 
         return mInstance;
     }
 
-    private GetImageHelper(boolean isCrop,Activity context)
-    {
+    private GetImageHelper(boolean isCrop, Activity context) {
         mContext = context;
         mIsCrop = isCrop;
-        rootFile= new File(context.getExternalCacheDir() + "/TakePhotoPic");
-        if (!rootFile.exists())
-        {
-            rootFile.mkdirs();
-        }
     }
 
-    public interface onTakeImageResult
-    {
+
+    public interface onTakeImageResult {
         void getImageResult(File imgeFile);
     }
 
-    public void popupGetImageDlg(Activity context, onTakeImageResult result)
-    {
+
+    public void popupGetImageDlg(Activity context, onTakeImageResult result) {
         mOnResult = result;
         new ActionSheetDialog(context)
                 .builder()
@@ -90,51 +69,31 @@ public class GetImageHelper
                 .addSheetItem("拍照", ActionSheetDialog.SheetItemColor.Blue,
                         new ActionSheetDialog.OnSheetItemClickListener() {
                             @Override
-                            public void onClick(int which)
-                            {
-//                                if (EasyPermissions.hasPermissions(mContext, PERMISSION_CAMERA, PERMISSION_WRITE)) {
-//                                    takePhoto();
-//                                } else {
-//                                    EasyPermissions.requestPermissions(((Activity)mContext), "need camera permission", REQUEST_PERMISSION_CAMERA, PERMISSION_CAMERA, PERMISSION_WRITE);
-//                                }
+                            public void onClick(int which) {
                                 takePhoto();
                             }
                         })
                 .addSheetItem("相册", ActionSheetDialog.SheetItemColor.Blue,
                         new ActionSheetDialog.OnSheetItemClickListener() {
                             @Override
-                            public void onClick(int which)
-                            {
-//                                if (EasyPermissions.hasPermissions(mContext, PERMISSION_WRITE)) {
-//                                    choosePhoto();
-//                                } else {
-//                                    EasyPermissions.requestPermissions(((Activity)mContext), "need camera permission", REQUEST_PERMISSION_WRITE, PERMISSION_WRITE);
-//                                }
+                            public void onClick(int which) {
                                 choosePhoto();
                             }
                         }).show();
     }
 
 
-
     private void takePhoto() {
-        //用于保存调用相机拍照后所生成的文件
-        if (!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
-            return;
-        }
-        captureFile = new File(rootFile, "temp.jpg");
-        //跳转到调用系统相机
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        //判断版本 如果在Android7.0以上,使用FileProvider获取Uri
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            intent.setFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
-            Uri contentUri = FileProvider.getUriForFile(mContext,mContext.getPackageName()+".fileprovider", captureFile);
-            intent.putExtra(MediaStore.EXTRA_OUTPUT, contentUri);
-        } else {
-            //否则使用Uri.fromFile(file)方法获取Uri
-            intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(captureFile));
-        }
-        mContext.startActivityForResult(intent, REQUEST_PERMISSION_CAMERA);
+
+        //打开照相机
+        Intent openCameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        mTakePhotoUri = PickUtils.getOutputMediaFileUri(mContext, "temp.jpg");
+        openCameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, mTakePhotoUri);
+
+        //Android7.0添加临时权限标记，此步千万别忘了
+        openCameraIntent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+        mContext.startActivityForResult(openCameraIntent, REQUEST_PERMISSION_CAMERA);
+
     }
 
 
@@ -145,21 +104,21 @@ public class GetImageHelper
     }
 
 
-    private  String getRealFilePath( final Context context, final Uri uri ) {
-        if ( null == uri ) return null;
+    private String getRealFilePath(final Context context, final Uri uri) {
+        if (null == uri) return null;
         final String scheme = uri.getScheme();
         String data = null;
-        if ( scheme == null )
+        if (scheme == null)
             data = uri.getPath();
-        else if ( ContentResolver.SCHEME_FILE.equals( scheme ) ) {
+        else if (ContentResolver.SCHEME_FILE.equals(scheme)) {
             data = uri.getPath();
-        } else if ( ContentResolver.SCHEME_CONTENT.equals( scheme ) ) {
-            Cursor cursor = context.getContentResolver().query( uri, new String[] { MediaStore.Images.ImageColumns.DATA }, null, null, null );
-            if ( null != cursor ) {
-                if ( cursor.moveToFirst() ) {
-                    int index = cursor.getColumnIndex( MediaStore.Images.ImageColumns.DATA );
-                    if ( index > -1 ) {
-                        data = cursor.getString( index );
+        } else if (ContentResolver.SCHEME_CONTENT.equals(scheme)) {
+            Cursor cursor = context.getContentResolver().query(uri, new String[]{MediaStore.Images.ImageColumns.DATA}, null, null, null);
+            if (null != cursor) {
+                if (cursor.moveToFirst()) {
+                    int index = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+                    if (index > -1) {
+                        data = cursor.getString(index);
                     }
                 }
                 cursor.close();
@@ -169,39 +128,16 @@ public class GetImageHelper
     }
 
 
-//    private String getPathFromUri(Uri uri, Context context) {
-//        if (uri == null) {
-//            return null;
-//        }
-//
-//        if (uri.getAuthority().compareToIgnoreCase("com.newasia.MyCar")==0)
-//        {
-//            return Environment.getExternalStorageDirectory()+uri.getPath().replace("/external_path", "");
-//        }
-//        else
-//        {
-//            return getRealFilePath(context,uri);
-//        }
-//    }
-
-
-
     /**
      * 裁剪图片
      */
-    private void cropPhoto(Uri uri)
-    {
-        if (!mIsCrop)
-        {
-            String strPath = getRealFilePath(mContext,uri);
-            if (strPath !=null)
-            {
-                mOnResult.getImageResult(new File(strPath));
-            }
+    private void cropPhoto(Uri uri) {
 
-            return;
-        }
-        cropFile = new File(rootFile, "avatar.jpg");
+        mCorpUri = PickUtils.getOutputMediaFileUri(mContext,"corp.jpg");
+
+
+
+
         Intent intent = new Intent("com.android.camera.action.CROP");
         intent.setDataAndType(uri, "image/*");
         intent.putExtra("crop", "true");
@@ -210,54 +146,40 @@ public class GetImageHelper
         intent.putExtra("outputX", 800);
         intent.putExtra("outputY", 800);
         intent.putExtra("return-data", false);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(cropFile));
-        intent.putExtra("outputFormat", Bitmap.CompressFormat.PNG.toString());
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, mCorpUri);
+        intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());
         intent.putExtra("noFaceDetection", true);
         intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
         intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        intent.setClipData(ClipData.newRawUri(MediaStore.EXTRA_OUTPUT, uri));
+
+        List<ResolveInfo> resInfoList =  mContext.getPackageManager()
+                .queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+        for (ResolveInfo resolveInfo : resInfoList) {
+            String packageName = resolveInfo.activityInfo.packageName;
+            mContext.grantUriPermission(packageName, mCorpUri, Intent.FLAG_GRANT_READ_URI_PERMISSION
+                    | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+        }
+
+
         mContext.startActivityForResult(intent, CROP_REQUEST_CODE);
     }
 
-
-
-
-    /**
-     * @param path
-     * @return
-     */
-    private String saveImage(String path) {
-        if (!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
-            return null;
-        }
-        Bitmap bitmap = BitmapFactory.decodeFile(path);
-        try {
-            FileOutputStream fos = new FileOutputStream(cropFile);
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
-            fos.flush();
-            fos.close();
-            return cropFile.getAbsolutePath();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
 
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode == RESULT_OK) {
             switch (requestCode) {
                 case REQUEST_PERMISSION_CAMERA:
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                        Uri contentUri = FileProvider.getUriForFile(mContext, mContext.getPackageName()+".fileprovider", captureFile);
-                        cropPhoto(contentUri);
-                    } else {
-                        cropPhoto(Uri.fromFile(captureFile));
-                    }
+                    if(mIsCrop)
+                    {
+                        cropPhoto(mTakePhotoUri);
+                    }else retrunResult(mTakePhotoUri);
                     break;
                 case REQUEST_PERMISSION_WRITE:
                     cropPhoto(data.getData());
                     break;
                 case CROP_REQUEST_CODE:
-                    mOnResult.getImageResult(cropFile);
+                    retrunResult(mCorpUri);
                     break;
                 default:
                     break;
@@ -265,5 +187,11 @@ public class GetImageHelper
         }
     }
 
+
+    private void retrunResult(Uri uri)
+    {
+        String path = PickUtils.getPath(mContext,uri);
+        mOnResult.getImageResult(new File(path));
+    }
 
 }
---
+++ BaseInputAdapter/src/main/java/com/newasia/baseinputadapter/PickUtils.java
@@ -0,0 +1,392 @@
+package com.newasia.baseinputadapter;
+
+
+import android.annotation.SuppressLint;
+import android.content.ContentUris;
+import android.content.Context;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.provider.OpenableColumns;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
+
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+
+public class PickUtils {
+
+    public static final String DOCUMENTS_DIR = "documents";
+
+    @SuppressLint("NewApi")
+    public static String getPath(final Context context, final Uri uri) {
+
+        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+
+        // DocumentProvider
+        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+            // ExternalStorageProvider
+            if (isExternalStorageDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                if ("primary".equalsIgnoreCase(type)) {
+                    return Environment.getExternalStorageDirectory() + "/" + split[1];
+                }
+            }
+            // DownloadsProvider
+            else if (isDownloadsDocument(uri)) {
+
+                final String id = DocumentsContract.getDocumentId(uri);
+
+                if (id != null && id.startsWith("raw:")) {
+                    return id.substring(4);
+                }
+
+                String[] contentUriPrefixesToTry = new String[]{
+                        "content://downloads/public_downloads",
+                        "content://downloads/my_downloads"
+                };
+
+                for (String contentUriPrefix : contentUriPrefixesToTry) {
+                    Uri contentUri = ContentUris.withAppendedId(Uri.parse(contentUriPrefix), Long.valueOf(id));
+                    try {
+                        String path = getDataColumn(context, contentUri, null, null);
+                        if (path != null && !path.equals("")) {
+                            return path;
+                        }
+                    } catch (Exception e) {
+                    }
+                }
+
+                // path could not be retrieved using ContentResolver, therefore copy file to accessible cache using streams
+                String fileName = getFileName(context, uri);
+                File cacheDir = getDocumentCacheDir(context);
+                File file = generateFileName(fileName, cacheDir);
+                String destinationPath = null;
+                if (file != null) {
+                    destinationPath = file.getAbsolutePath();
+                    saveFileFromUri(context, uri, destinationPath);
+                }
+
+                return destinationPath;
+            }
+            // MediaProvider
+            else if (isMediaDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                Uri contentUri = null;
+                if ("image".equals(type)) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+
+                final String selection = "_id=?";
+                final String[] selectionArgs = new String[]{split[1]};
+
+                return getDataColumn(context, contentUri, selection, selectionArgs);
+            }
+        }
+        // MediaStore (and general)
+        else if ("content".equalsIgnoreCase(uri.getScheme())) {
+            String path = getDataColumn(context, uri, null, null);
+            if (path != null && !path.equals("")) return path;
+
+            // path could not be retrieved using ContentResolver, therefore copy file to accessible cache using streams
+            String fileName = getFileName(context, uri);
+            File cacheDir = getDocumentCacheDir(context);
+            File file = generateFileName(fileName, cacheDir);
+            String destinationPath = null;
+            if (file != null) {
+                destinationPath = file.getAbsolutePath();
+                saveFileFromUri(context, uri, destinationPath);
+            }
+            return destinationPath;
+        }
+        // File
+        else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+        return null;
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is ExternalStorageProvider.
+     */
+    public static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is DownloadsProvider.
+     */
+    public static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is MediaProvider.
+     */
+    public static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
+    }
+
+    public static String getDataColumn(Context context, Uri uri, String selection,
+                                       String[] selectionArgs) {
+
+        Cursor cursor = null;
+        final String column = "_data";
+        final String[] projection = {column};
+        String path = "";
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
+                    null);
+            if (cursor != null && cursor.moveToFirst()) {
+                final int column_index = cursor.getColumnIndexOrThrow(column);
+                path = cursor.getString(column_index);
+                return path;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return path;
+    }
+
+    public static String getFileName(@NonNull Context context, Uri uri) {
+        String mimeType = context.getContentResolver().getType(uri);
+        String filename = null;
+
+        if (mimeType == null && context != null) {
+            String path = getPath(context, uri);
+            if (path == null) {
+                filename = getName(uri.toString());
+            } else {
+                File file = new File(path);
+                filename = file.getName();
+            }
+        } else {
+            Cursor returnCursor = context.getContentResolver().query(uri, null,
+                    null, null, null);
+            if (returnCursor != null) {
+                int nameIndex = returnCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+                returnCursor.moveToFirst();
+                filename = returnCursor.getString(nameIndex);
+                returnCursor.close();
+            }
+        }
+
+        return filename;
+    }
+
+    public static String getName(String filename) {
+        if (filename == null) {
+            return null;
+        }
+        int index = filename.lastIndexOf('/');
+        return filename.substring(index + 1);
+    }
+
+    public static File getDocumentCacheDir(@NonNull Context context) {
+        File dir = new File(context.getCacheDir(), DOCUMENTS_DIR);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+
+        return dir;
+    }
+
+    @Nullable
+    public static File generateFileName(@Nullable String name, File directory) {
+        if (name == null) {
+            return null;
+        }
+
+        File file = new File(directory, name);
+
+        if (file.exists()) {
+            String fileName = name;
+            String extension = "";
+            int dotIndex = name.lastIndexOf('.');
+            if (dotIndex > 0) {
+                fileName = name.substring(0, dotIndex);
+                extension = name.substring(dotIndex);
+            }
+
+            int index = 0;
+
+            while (file.exists()) {
+                index++;
+                name = fileName + '(' + index + ')' + extension;
+                file = new File(directory, name);
+            }
+        }
+
+        try {
+            if (!file.createNewFile()) {
+                return null;
+            }
+        } catch (IOException e) {
+            return null;
+        }
+
+        return file;
+    }
+
+
+    private static void saveFileFromUri(Context context, Uri uri, String destinationPath) {
+        InputStream is = null;
+        BufferedOutputStream bos = null;
+        try {
+            is = context.getContentResolver().openInputStream(uri);
+            bos = new BufferedOutputStream(new FileOutputStream(destinationPath, false));
+            byte[] buf = new byte[1024];
+            is.read(buf);
+            do {
+                bos.write(buf);
+            } while (is.read(buf) != -1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (is != null) is.close();
+                if (bos != null) bos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    public static Bitmap getBitmapFormUri(Context context, Uri uri) throws FileNotFoundException, IOException {
+
+        InputStream input = new FileInputStream(getPath(context,uri));
+
+        //这一段代码是不加载文件到内存中也得到bitmap的真是宽高，主要是设置inJustDecodeBounds为true
+        BitmapFactory.Options onlyBoundsOptions = new BitmapFactory.Options();
+        onlyBoundsOptions.inJustDecodeBounds = true;//不加载到内存
+        onlyBoundsOptions.inDither = true;//optional
+        onlyBoundsOptions.inPreferredConfig = Bitmap.Config.RGB_565;//optional
+        BitmapFactory.decodeStream(input, null, onlyBoundsOptions);
+        input.close();
+        int originalWidth = onlyBoundsOptions.outWidth;
+        int originalHeight = onlyBoundsOptions.outHeight;
+        if ((originalWidth == -1) || (originalHeight == -1))
+            return null;
+
+        //图片分辨率以480x800为标准
+        float hh = 800f;//这里设置高度为800f
+        float ww = 480f;//这里设置宽度为480f
+        //缩放比，由于是固定比例缩放，只用高或者宽其中一个数据进行计算即可
+        int be = 1;//be=1表示不缩放
+        if (originalWidth > originalHeight && originalWidth > ww) {//如果宽度大的话根据宽度固定大小缩放
+            be = (int) (originalWidth / ww);
+        } else if (originalWidth < originalHeight && originalHeight > hh) {//如果高度高的话根据宽度固定大小缩放
+            be = (int) (originalHeight / hh);
+        }
+        if (be <= 0)
+            be = 1;
+        //比例压缩
+        BitmapFactory.Options bitmapOptions = new BitmapFactory.Options();
+        bitmapOptions.inSampleSize = be;//设置缩放比例
+        bitmapOptions.inDither = true;
+        bitmapOptions.inPreferredConfig = Bitmap.Config.RGB_565;
+        input = context.getContentResolver().openInputStream(uri);
+        Bitmap bitmap = BitmapFactory.decodeStream(input, null, bitmapOptions);
+        input.close();
+
+        return compressImage(bitmap);//再进行质量压缩
+    }
+
+
+    public static Bitmap compressImage(Bitmap image) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        image.compress(Bitmap.CompressFormat.JPEG, 100, baos);//质量压缩方法，这里100表示不压缩，把压缩后的数据存放到baos中
+        int options = 100;
+        while (baos.toByteArray().length / 1024 > 100) {  //循环判断如果压缩后图片是否大于100kb,大于继续压缩
+            baos.reset();//重置baos即清空baos
+            //第一个参数 ：图片格式 ，第二个参数： 图片质量，100为最高，0为最差  ，第三个参数：保存压缩后的数据的流
+            image.compress(Bitmap.CompressFormat.JPEG, options, baos);//这里压缩options，把压缩后的数据存放到baos中
+            options -= 10;//每次都减少10
+            if (options <= 0)
+                break;
+        }
+        ByteArrayInputStream isBm = new ByteArrayInputStream(baos.toByteArray());//把压缩后的数据baos存放到ByteArrayInputStream中
+        Bitmap bitmap = BitmapFactory.decodeStream(isBm, null, null);//把ByteArrayInputStream数据生成图片
+        return bitmap;
+    }
+
+
+
+    public static Uri getOutputMediaFileUri(Context context,String name) {
+        File mediaFile = null;
+        String cameraPath;
+        try {
+            File mediaStorageDir = new File(context.getExternalCacheDir().getAbsolutePath()+File.separator+"Pictures");
+            if (!mediaStorageDir.exists()) {
+                if (!mediaStorageDir.mkdirs()) {
+                    return null;
+                }
+            }
+            mediaFile = new File(mediaStorageDir.getPath()
+                    + File.separator
+                    + name);
+            if(mediaFile.exists()){
+                mediaFile.delete();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {// sdk >= 24  android7.0以上
+            Uri contentUri = FileProvider.getUriForFile(context,
+                    context.getApplicationContext().getPackageName() + ".fileProvider",//与清单文件中android:authorities的值保持一致
+                    mediaFile);//FileProvider方式或者ContentProvider。也可使用VmPolicy方式
+            Log.e("test",contentUri.toString());
+            return contentUri;
+
+        } else {
+            return Uri.fromFile(mediaFile);//或者 Uri.isPaise("file://"+file.toString()
+
+        }
+    }
+
+
+    public static void deleteFile(Context context,Uri uri)
+    {
+        File delFile = new File(getPath(context,uri));
+        if(delFile.exists())
+        {
+            delFile.delete();
+        }
+    }
+
+}
